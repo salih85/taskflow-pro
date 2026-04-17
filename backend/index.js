0694cbc8b4059a3f8a4a1269bb71002bc185e21a
Mohammed salih.k <muhammedansaransar83@gmail.com>
2026-04-17 10:26:32 -0400

    first commit

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = express()
 const connectDB = require('./config/db')
 
 
+
 const port  = process.env.PORT || 3000
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
